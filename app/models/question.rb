934da1b01d1d97ccb5aadb4f7cc6ee055caf70fb
fokayx <fokayx@gmail.com>
2015-04-13 10:48:42 +0800

    add relation to models: participant, survey, question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,12 @@
 class Question < ActiveRecord::Base
   belongs_to :survery
+  has_many :answers
+  has_many :participants, through: :answers
+
+  accepts_nested_attributes_for :answers
+end
+
+class Answer < ActiveRecord::Base
+  belongs_to :participant
+  belongs_to :question
 end
